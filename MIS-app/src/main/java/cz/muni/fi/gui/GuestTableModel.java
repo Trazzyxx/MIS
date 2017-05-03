cbbe12e09d68b92bb04feb68a786df530698e14b
Vladimir Mecko <vladimir.mecko@gmail.com>
2017-05-03 18:55:37 +0200

    MIS 1.5

--- MIS-app/src/main/java/cz/muni/fi/gui/GuestTableModel.java
+++ MIS-app/src/main/java/cz/muni/fi/gui/GuestTableModel.java
@@ -1,47 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cz.muni.fi.gui;
 
+import cz.muni.fi.MIS.Guest;
+import cz.muni.fi.MIS.GuestManager;
 import cz.muni.fi.MIS.Main;
-import cz.muni.fi.MIS.Room;
-import cz.muni.fi.MIS.RoomManager;
-
-import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javax.swing.SwingWorker;
+import javax.swing.table.AbstractTableModel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
- * Created by V.Mecko on 3.5.2017.
+ *
+ * @author Vladko
  */
-public class RoomsTableModel extends AbstractTableModel {
+public class GuestTableModel extends AbstractTableModel{
     private static final ResourceBundle texts = ResourceBundle.getBundle("texts");
-    public static final int ROOM_COLUMNS = 3;
+    public static final int GUEST_COLUMN = 4;
+    private GuestManager guestManager;
     
-    protected ApplicationContext ctx;
-    protected RoomManager roomManager;
-    protected List<Room> rooms = new ArrayList<>();
+    private List<Guest> guests = new ArrayList<>();
 
-    public RoomsTableModel() {
-        ctx = new AnnotationConfigApplicationContext(Main.SpringConfig.class); 
-        roomManager = ctx.getBean(texts.getString("roomManager"), RoomManager.class);
+    private class RetrieveSwingWorker extends SwingWorker<Void,Void> {        
+        @Override    
+        protected Void doInBackground() throws Exception {
+            guests = guestManager.listAllGuests();
+            return null;
+        }
+        
+        @Override    
+        protected void done() {
+            fireTableRowsInserted(0, getRowCount() - 1);
+        }
+     }
+    
+    public GuestTableModel() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(Main.SpringConfig.class);        
+        guestManager = ctx.getBean(texts.getString("guestManager"), GuestManager.class);
         
         RetrieveSwingWorker retrieveSwingWorker = new RetrieveSwingWorker();
         retrieveSwingWorker.execute();
     }
     
     private class CreateSwingWorker extends SwingWorker<Void,Void> {
-        private Room room;
+        private Guest guest;
         
-        public CreateSwingWorker(Room room) {
-            this.room = room;
+        public CreateSwingWorker(Guest guest) {
+            this.guest = guest;
         }
         
         @Override    
         protected Void doInBackground() throws Exception {
-            roomManager.createRoom(room);
-            rooms.add(room);
+            guestManager.createGuest(guest);
+            guests.add(guest);
             return null;
         }
         
@@ -52,34 +69,21 @@ public class RoomsTableModel extends AbstractTableModel {
         }
     }
     
-    private class RetrieveSwingWorker extends SwingWorker<Void,Void> {        
-        @Override    
-        protected Void doInBackground() throws Exception {
-            rooms = roomManager.findAllRooms();
-            return null;
-        }
-        
-        @Override    
-        protected void done() {
-            fireTableRowsInserted(0, getRowCount() - 1);
-        }
-    }
-    
     private class UpdateSwingWorker extends SwingWorker<Void,Void> {
-        private Room room;
+        private Guest guest;
         private int rowIndex;
         private int columnIndex;
         
-        public UpdateSwingWorker(Room room, int rowIndex, int columnIndex) {
-            this.room = room;
+        public UpdateSwingWorker(Guest guest, int rowIndex, int columnIndex) {
+            this.guest = guest;
             this.rowIndex = rowIndex;
             this.columnIndex = columnIndex;
         }
         
         @Override    
         protected Void doInBackground() throws Exception {
-            roomManager.updateRoom(room);
-            rooms.set(rowIndex, room);
+            guestManager.updateGuest(guest);
+            guests.set(rowIndex, guest);
             return null;
         }
         
@@ -98,9 +102,9 @@ public class RoomsTableModel extends AbstractTableModel {
         
         @Override    
         protected Void doInBackground() throws Exception {
-            Room roomForDelete = rooms.get(row);
-            rooms.remove(row);
-            roomManager.deleteRoom(roomForDelete);
+            Guest guest = guests.get(row);
+            guests.remove(row);
+            guestManager.deleteGuest(guest);
             return null;
         }
         
@@ -109,29 +113,31 @@ public class RoomsTableModel extends AbstractTableModel {
             fireTableRowsDeleted(row, row);
         }
     }
- 
+    
     @Override
     public int getRowCount() {
-        return rooms.size();
+        return guests.size();
     }
  
     @Override
     public int getColumnCount() {
-        return ROOM_COLUMNS;
+        return GUEST_COLUMN;
     }
  
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        Room room = rooms.get(rowIndex);
+        Guest guest = guests.get(rowIndex);
         switch (columnIndex) {
             case 0:
-                return room.getRoomID();
+                return guest.getGuestID();
             case 1:
-                return room.getCapacity();
+                return guest.getPhoneNumber();
             case 2:
-                return room.getRoomNumber();
+                return guest.getAddress();
+            case 3:
+                return guest.getFullName();
             default:
-                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+                throw new IllegalArgumentException(texts.getString("wrong column index"));
         }
     }
     
@@ -141,11 +147,13 @@ public class RoomsTableModel extends AbstractTableModel {
             case 0:
                 return texts.getString("id");
             case 1:
-                return texts.getString("capacity");
+                return texts.getString("phoneNumber");
             case 2:
-                return texts.getString("roomNumber");
+                return texts.getString("address");
+            case 3:
+                return texts.getString("fullname");
             default:
-                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+                throw new IllegalArgumentException(texts.getString("wrong column index"));
         }
     }
     
@@ -155,31 +163,36 @@ public class RoomsTableModel extends AbstractTableModel {
             case 0:
                 return Long.class;
             case 1:
-                return Integer.class;
+                return String.class;
             case 2:
                 return String.class;
+            case 3:
+                return String.class;
             default:
-                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+                throw new IllegalArgumentException(texts.getString("wrong column index"));
         }
     }
     
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        Room room = rooms.get(rowIndex);
+        Guest guest = guests.get(rowIndex); // swingworker
         switch (columnIndex) {
             case 0:
-                room.setRoomID((Long) aValue);
+                guest.setGuestID((Long) aValue);
                 break;
             case 1:
-                room.setCapacity((Integer) aValue);
+                guest.setPhoneNumber((String) aValue);
                 break;
             case 2:
-                room.setRoomNumber((String) aValue);
+                guest.setAddress((String) aValue);
+                break;
+            case 3:
+                guest.setFullName((String) aValue);
                 break;
             default:
-                throw new IllegalArgumentException(texts.getString("Working with bad column index."));
+                throw new IllegalArgumentException(texts.getString("wrong column index"));
         }
-        UpdateSwingWorker updateSwingWorker = new UpdateSwingWorker(room, rowIndex, columnIndex);
+        UpdateSwingWorker updateSwingWorker = new UpdateSwingWorker(guest, rowIndex, columnIndex);
         updateSwingWorker.execute();
     }
 
@@ -190,14 +203,15 @@ public class RoomsTableModel extends AbstractTableModel {
                 return false;
             case 1:
             case 2:
+            case 3:
                 return true;
             default:
-                throw new IllegalArgumentException(texts.getString("Working with bad column index."));
+                throw new IllegalArgumentException(texts.getString("wrong column index."));
         }
     }
     
-    public void addRow(Room room) {
-        CreateSwingWorker createSwingWorker = new CreateSwingWorker(room);
+    public void addRow(Guest guest) {
+        CreateSwingWorker createSwingWorker = new CreateSwingWorker(guest);
         createSwingWorker.execute();
     }
     
@@ -206,4 +220,9 @@ public class RoomsTableModel extends AbstractTableModel {
         deleteSwingWorker.execute();
     }
     
-}
+    public Guest getRow(int row) {
+        return guests.get(row);
+    }
+    
+ }
+
---
+++ MIS-app/src/main/java/cz/muni/fi/gui/ReservationTableModel.java
@@ -0,0 +1,278 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.gui;
+
+import cz.muni.fi.MIS.Main;
+import cz.muni.fi.MIS.Reservation;
+import cz.muni.fi.MIS.ReservationManager;
+import cz.muni.fi.MIS.Room;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.swing.SwingWorker;
+import javax.swing.table.AbstractTableModel;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ *
+ * @author Vladko
+ */
+public class ReservationTableModel extends AbstractTableModel {
+    private static final ResourceBundle texts = ResourceBundle.getBundle("texts");
+    public static final int RSRVTION_COLUMNS = 9;
+    
+    protected ApplicationContext ctx;
+    protected ReservationManager reservationManager;
+    protected List<Reservation> reservations = new ArrayList<>();
+
+    public ReservationTableModel() {
+        ctx = new AnnotationConfigApplicationContext(Main.SpringConfig.class); 
+        reservationManager = ctx.getBean(texts.getString("reservationManager"), ReservationManager.class);
+        
+        RetrieveSwingWorker retrieveSwingWorker = new RetrieveSwingWorker();
+        retrieveSwingWorker.execute();
+    }
+    
+    private class CreateSwingWorker extends SwingWorker<Void,Void> {
+        private Reservation reservation;
+        
+        public CreateSwingWorker(Reservation reservation) {
+            this.reservation = reservation;
+        }
+        
+        @Override    
+        protected Void doInBackground() throws Exception {
+            reservationManager.createReservation(reservation);
+            reservations.add(reservation);
+            return null;
+        }
+        
+        @Override    
+        protected void done() {
+            int lastRow = getRowCount() - 1;
+            fireTableRowsInserted(lastRow, lastRow);
+        }
+    }
+    
+    private class RetrieveSwingWorker extends SwingWorker<Void,Void> {        
+        @Override    
+        protected Void doInBackground() throws Exception {
+            reservations = reservationManager.findAllReservations();
+            return null;
+        }
+        
+        @Override    
+        protected void done() {
+            fireTableRowsInserted(0, getRowCount() - 1);
+        }
+    }
+    
+    private class UpdateSwingWorker extends SwingWorker<Void,Void> {
+        private Reservation reservation;
+        private int rowIndex;
+        private int columnIndex;
+        
+        public UpdateSwingWorker(Reservation reservation, int rowIndex, int columnIndex) {
+            this.reservation = reservation;
+            this.rowIndex = rowIndex;
+            this.columnIndex = columnIndex;
+        }
+        
+        @Override    
+        protected Void doInBackground() throws Exception {
+            reservationManager.updateReservation(reservation);
+            reservations.set(rowIndex, reservation);
+            return null;
+        }
+        
+        @Override    
+        protected void done() {
+            fireTableCellUpdated(rowIndex, columnIndex);
+        }
+    }
+    
+    private class DeleteSwingWorker extends SwingWorker<Void,Void> {        
+        private int row;
+        
+        public DeleteSwingWorker(int row) {
+            this.row = row;
+        }
+        
+        @Override    
+        protected Void doInBackground() throws Exception {
+            Reservation ResForDelete = reservations.get(row);
+            reservations.remove(row);
+            reservationManager.deleteReservation(ResForDelete);
+            return null;
+        }
+        
+        @Override    
+        protected void done() {
+            fireTableRowsDeleted(row, row);
+        }
+    }
+ 
+    @Override
+    public int getRowCount() {
+        return reservations.size();
+    }
+ 
+    @Override
+    public int getColumnCount() {
+        return RSRVTION_COLUMNS;
+    }
+ 
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Reservation reservation = reservations.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return reservation.getReservationID();
+            case 1:
+                return reservation.getStartTime();
+            case 2:
+                return reservation.getEndTime();
+            case 3:
+                return reservation.getRoom().getCapacity();
+            case 4:
+                return reservation.getRoom().getRoomNumber();
+            case 5:
+                return reservation.getGuest().getPhoneNumber();
+            case 6:
+                return reservation.getGuest().getAddress();
+            case 7:
+                return reservation.getGuest().getFullName();
+            case 8:
+                return reservation.getPrice();
+           default:
+                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+        }
+    }
+    
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return texts.getString("id");
+            case 1:
+                return texts.getString("startTime");
+            case 2:
+                return texts.getString("endTime");
+            case 3:
+                return texts.getString("capacity");
+            case 4:
+                return texts.getString("roomNumber");
+            case 5:
+                return texts.getString("phoneNumber");
+            case 6:
+                return texts.getString("address");
+            case 7:
+                return texts.getString("fullname");
+            case 8:
+                return texts.getString("price");
+            default:
+                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+        }
+    }
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return Long.class;
+            case 1:
+                return LocalDate.class;
+            case 2:
+                return LocalDate.class;
+            case 3:
+                return Integer.class;
+            case 4:
+                return String.class;
+            case 5:
+                return String.class;
+            case 6:
+                return String.class;
+            case 7:
+                return String.class;
+            case 8:
+                return BigDecimal.class;
+            default:
+                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+        }
+    }
+    
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        Reservation reservation = reservations.get(rowIndex);
+          switch (columnIndex) {
+            case 0:
+                reservation.setReservationID((Long) aValue);
+                break;
+            case 1:
+                reservation.setStartTime((LocalDate) aValue);
+                break;
+            case 2:
+                reservation.setEndTime((LocalDate) aValue);
+                break;
+            case 3:
+                reservation.getRoom().setCapacity((Integer) aValue);
+                break;
+            case 4:
+                 reservation.getRoom().setRoomNumber((String)aValue);
+                 break;
+            case 5:
+                reservation.getGuest().setPhoneNumber((String)aValue);
+                break;
+            case 6:
+                 reservation.getGuest().setAddress((String)aValue);
+                 break;
+            case 7:
+                 reservation.getGuest().setFullName((String)aValue);
+                 break;
+            case 8:
+                 reservation.setPrice((BigDecimal) aValue);
+                 break;
+            default:
+                throw new IllegalArgumentException(texts.getString("working with bad column index."));
+          }
+       
+        UpdateSwingWorker updateSwingWorker = new UpdateSwingWorker(reservation, rowIndex, columnIndex);
+        updateSwingWorker.execute();
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return false;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return true;
+            default:
+                throw new IllegalArgumentException(texts.getString("Working with bad column index."));
+        }
+    }
+    
+    public void addRow(Reservation reservation) {
+        CreateSwingWorker createSwingWorker = new CreateSwingWorker(reservation);
+        createSwingWorker.execute();
+    }
+    
+    public void removeRow(int row) {
+        DeleteSwingWorker deleteSwingWorker = new DeleteSwingWorker(row);
+        deleteSwingWorker.execute();
+    }
+    
+}
